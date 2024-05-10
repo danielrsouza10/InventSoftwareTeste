b4b7e1d37e06f60ac999326da3aeb2e183c0133f
dev-macb <dev.macb@gmail.com>
2024-05-10 12:15:04 -0300

    Adicionar exercício 1159 - Árvore Binária

---
+++ 1159-arvore-binaria-miguel/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace App {
+    internal class Program {
+        static void Main(string[] args) {
+            List<ArvoreBinaria> floresta = new List<ArvoreBinaria>();
+
+            int quantidadeTeste = int.Parse(Console.ReadLine());
+            for (int i = 0; i < quantidadeTeste; i++) {
+                ArvoreBinaria arvoreAtual = new ArvoreBinaria();
+
+                int quantidadeNos = int.Parse(Console.ReadLine());
+                string[] valorNos = Console.ReadLine().Split(' ');
+                for (int j = 0; j < quantidadeNos; j++) arvoreAtual.Inserir(int.Parse(valorNos[j]));
+
+                floresta.Add(arvoreAtual);
+            }
+
+            int contador = 1;
+            foreach (ArvoreBinaria arvore in floresta) {
+                Console.WriteLine($"Case {contador}:");
+                Console.Write("Pre.: ");
+                arvore.ImprimirPreOrdem(arvore.Raiz);
+                Console.Write("\nIn..: ");
+                arvore.ImprimirOrdem(arvore.Raiz);
+                Console.Write("\nPost: ");
+                arvore.ImprimirPosOrdem(arvore.Raiz);
+                Console.WriteLine("\n");
+                contador++;
+            }
+        }
+    }
+
+    public class No {
+        public int Valor;
+        public No? Direita;
+        public No? Esquerda;
+
+        public No(int valor) {
+            Valor = valor;
+            Direita = null;
+            Esquerda = null;
+        }
+    }
+
+    public class ArvoreBinaria {
+        public No? Raiz;
+
+        public ArvoreBinaria() {
+            Raiz = null;
+        }
+
+        public void Inserir(int valor) {
+            No novoNo = new No(valor);
+            
+            if (Raiz == null) {
+                Raiz = novoNo;
+            }
+            else {
+                No noAtual = Raiz;
+                
+                while (true) {
+                    if (valor < noAtual.Valor) {
+                        if (noAtual.Esquerda == null) {
+                            noAtual.Esquerda = novoNo;
+                            break;
+                        }
+                        noAtual = noAtual.Esquerda;
+                    }
+                    else {
+                        if (noAtual.Direita == null) {
+                            noAtual.Direita = novoNo;
+                            break;
+                        }
+                        noAtual = noAtual.Direita;
+                    }
+                }
+            }
+        }
+
+        public void ImprimirPreOrdem(No Raiz) {
+            if (Raiz != null) {
+                Console.Write($"{Raiz.Valor} ");
+                ImprimirPreOrdem(Raiz.Esquerda);
+                ImprimirPreOrdem(Raiz.Direita);
+            }
+        }
+
+        public void ImprimirOrdem(No Raiz) {
+            if (Raiz != null) {
+                ImprimirPreOrdem(Raiz.Esquerda);
+                Console.Write($"{Raiz.Valor} ");
+                ImprimirPreOrdem(Raiz.Direita);
+            }
+        }
+
+        public void ImprimirPosOrdem(No Raiz) {
+            if (Raiz != null) {
+                ImprimirPreOrdem(Raiz.Esquerda);
+                ImprimirPreOrdem(Raiz.Direita);
+                Console.Write($"{Raiz.Valor} ");
+            }
+        }
+    }
+
+}
+
+
+
+
+
